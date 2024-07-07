14404ce488079a95107069c48d466db1ecb11593
Raul E. Paneque <rlraul12@gmail.com>
2024-07-07 14:16:42 +0200

    post, delete

--- MyAPI/urls.py
+++ MyAPI/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 # Importando desde views.py la funcion obtener_productos()
+from . import views
 from .views import getAPI, obtener_productos
 
 urlpatterns = [
     path('', obtener_productos, name='obtener_productos'),
+    path('producto/<int:product_id>',views.product_detail, name='product_detail'),
+    path('product/<int:product_id>/', views.delete_product, name='delete_product'),
+
     path('api/', getAPI, name='getAPI'),
 ]
--- MyAPI/views.py
+++ MyAPI/views.py
@@ -1,4 +1,6 @@
+from django.http import JsonResponse
 from django.shortcuts import render, HttpResponse
+from django.views.decorators.http import require_http_methods
 # Importando Libreria Requests para hacer solicitudes HTTP
 import requests
 
@@ -38,3 +40,36 @@ def obtener_productos(request):
         productos = []
 
     return render(request, 'index.html', {'productos': productos})
+
+
+def product_detail(request, product_id):
+    URL_API = 'https://fakestoreapi.com/products/'
+  
+    # Construir la URL completa con el ID del producto
+    product_url = f'{URL_API}{product_id}'
+    
+    # Hacer la solicitud GET a la API
+    response = requests.get(product_url)
+
+    # Verificar si la solicitud fue exitosa
+    if response.status_code == 200:
+        # Convertir la respuesta en formato JSON
+        product_data = response.json()
+        return render(request, 'product_detail.html', {'producto': product_data})
+    else:
+        return JsonResponse({'error': 'Product not found'}, status=404)
+    
+@require_http_methods(["GET", "POST"])
+def delete_product(request, product_id):
+    URL_API = 'https://fakestoreapi.com/products/'
+    product_url = f'{URL_API}{product_id}'
+
+    if request.method == 'POST':
+        response = requests.delete(product_url)
+        if response.status_code == 200:
+            return JsonResponse({'message': 'Product deleted successfully'}, status=200)
+        else:
+            return JsonResponse({'error': 'Failed to delete product'}, status=response.status_code)
+
+    # Si el método es GET, simplemente renderiza la confirmación de eliminación
+    return render(request, 'confirm_delete.html', {'product_id': product_id})
